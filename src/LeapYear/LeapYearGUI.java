65b61501f6f210d803d8fbed4bed72294c0b30d9
StayFrosht <johnquinnvic.taboada@cit.edu>
2022-12-16 14:30:50 +0800

    Add exception handling to Leap Year

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -9,19 +9,29 @@ public class LeapYearGUI extends JFrame{
 
     public LeapYearGUI() {
         btnCheckYear.addActionListener(e -> {
-            int year = Integer.parseInt(tfYear.getText());
-            boolean isLeapYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-            JOptionPane.showMessageDialog(null, "The year " + year + " is " + (isLeapYear ? "a leap year." : "not a leap year."));
+            try {
+                int year = Integer.parseInt(tfYear.getText());
+                boolean isLeapYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+                JOptionPane.showMessageDialog(null, "The year " + year + " is " + (isLeapYear ? "a leap year." : "not a leap year."));
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(null, "An error occurred: " + ex.getMessage());
+            }
         });
+
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Leap Year Checker");
-        frame.setContentPane(new LeapYearGUI().panel1);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setVisible(true);
+        try {
+            JFrame frame = new JFrame("Leap Year Checker");
+            frame.setContentPane(new LeapYearGUI().panel1);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setVisible(true);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "An error occurred: " + e.getMessage());
+        }
     }
+
 }
 
 
