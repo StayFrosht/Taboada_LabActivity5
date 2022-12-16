6812f5faf4f0a776bc19fc23be1c683152d43725
StayFrosht <johnquinnvic.taboada@cit.edu>
2022-12-16 14:25:38 +0800

    SHOCKS FORGOT TO FOLLOW INSTRUCTIONS Here Add Leap Year main function!

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -2,12 +2,22 @@ package LeapYear;
 
 import javax.swing.*;
 
-public class LeapYearGUI {
+public class LeapYearGUI extends JFrame{
     private JPanel panel1;
     private JTextField tfYear;
     private JButton btnCheckYear;
 
+    public LeapYearGUI() {
 
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Leap Year Checker");
+        frame.setContentPane(new LeapYearGUI().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
 
 
