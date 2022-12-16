a5c602732a9a347c38147404e8aa29bbc840ca5b
StayFrosht <johnquinnvic.taboada@cit.edu>
2022-12-16 15:04:20 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,9 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
 
 public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
@@ -17,10 +20,43 @@ public class FoodOrderGUI extends JFrame{
     private JRadioButton rb15;
 
     public FoodOrderGUI() {
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double totalPrice = 0;
+                if (cPizza.isSelected()) {
+                    totalPrice += 100;
+                }
+                if (cBurger.isSelected()) {
+                    totalPrice += 80;
+                }
+                if (cFries.isSelected()) {
+                    totalPrice += 65;
+                }
+                if (cSoftDrinks.isSelected()) {
+                    totalPrice += 55;
+                }
+                if (cTea.isSelected()) {
+                    totalPrice += 50;
+                }
+                if (cSundae.isSelected()) {
+                    totalPrice += 40;
+                }
+                if (rb5.isSelected()) {
+                    totalPrice *= 0.95;
+                } else if (rb10.isSelected()) {
+                    totalPrice *= 0.9;
+                } else if (rb15.isSelected()) {
+                    totalPrice *= 0.85;
+                }
+                DecimalFormat df = new DecimalFormat("#.00");
+                JOptionPane.showMessageDialog(null, "The total price is Php " + df.format(totalPrice));
+            }
+        });
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Food Order GUI");
+        JFrame frame = new JFrame("Food Ordering System");
         frame.setContentPane(new FoodOrderGUI().panel1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
